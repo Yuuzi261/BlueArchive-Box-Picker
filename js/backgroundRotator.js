f576fccf2e0b0160fc4aa76d87b3ab14bb824f8d
FusyFairy <dd8611706@gmail.com>
2024-05-17 11:02:08 +0800

    refactor: support fade-out, fade-in

--- js/backgroundRotator.js
+++ js/backgroundRotator.js
@@ -1,37 +1,3 @@
-// // 定义背景图像列表
-// const backgrounds = [
-//     "/images/background/BG_MainOffice_Night2.jpg",
-//     "/images/background/BG_MainOffice_Table.png",
-//     "/images/background/BG_MainOffice_Table2.png",
-//     "/images/background/BG_MainOffice_Table3.png",
-//     "/images/background/BG_MainOffice_Night.jpg"
-// ];
-
-// const body = document.querySelector('body');
-
-// let currentIndex = 0;
-// const interval = 7; // unit: second
-
-// const preloadImage = (src) => {
-//     const img = new Image();
-//     img.src = src;
-// };
-
-// const switchBackground = () => {
-//     const newBackground = backgrounds[currentIndex];
-//     body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("${newBackground}")`;
-//     currentIndex = (currentIndex + 1) % backgrounds.length;
-//     preloadImage(backgrounds[currentIndex]);
-// };
-
-// // preload first image
-// preloadImage(backgrounds[currentIndex]);
-
-// setInterval(() => {
-//     switchBackground();
-// }, interval * 1000);
-
-
 const backgrounds = [
     "/images/background/BG_MainOffice_Night2.jpg",
     "/images/background/BG_MainOffice_Table.png",
@@ -40,39 +6,37 @@ const backgrounds = [
     "/images/background/BG_MainOffice_Night.jpg"
 ];
 
-const body = document.querySelector('body');
-const overlay = document.querySelector('.background-overlay');
+const body = document.querySelector('.bg_img');
 
 let currentIndex = 0;
-const interval = 10; // unit: second
+const interval = 7; // unit: second
+const transitionDuration = 2.3; // unit: second
 
-const preloadImage = (src) => {
+const preloadImage = (src) =>{
     const img = new Image();
     img.src = src;
 };
 
 const switchBackground = () => {
-    overlay.style.opacity = 0;
-
-    setTimeout(() => {
-        const newBackground = backgrounds[currentIndex];
-        overlay.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("${newBackground}")`;
-        overlay.style.opacity = 1;
-    }, 1000); // Duration of fade out transition (1 second)
-
+    body.classList.add('fade-out');
+    
     setTimeout(() => {
         const newBackground = backgrounds[currentIndex];
         body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("${newBackground}")`;
-        overlay.style.opacity = 0;
         currentIndex = (currentIndex + 1) % backgrounds.length;
         preloadImage(backgrounds[currentIndex]);
-    }, 2000)
 
+        body.classList.remove('fade-out');
+        body.classList.add('fade-in');
+
+        setTimeout(() => {
+            body.classList.remove('fade-in');
+            setTimeout(switchBackground, (interval - transitionDuration) * 1000);
+        }, transitionDuration * 1000); // same as opacity transition time in css
+    }, transitionDuration * 1000); // switch background after fade-out
 };
 
 // preload first image
 preloadImage(backgrounds[currentIndex]);
 
-setInterval(() => {
-    switchBackground();
-}, interval * 1000);
+setTimeout(switchBackground, interval * 1000);
