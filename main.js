96ebe1137231d69f33719a45a0a79f4daf2e73fa
Error404 <78064680+FuseFairy@users.noreply.github.com>
2024-08-11 12:52:19 +0800

    Revert "feat: layout adjustment"

--- main.js
+++ main.js
@@ -161,7 +161,6 @@ function initializeApp(roles, state_maps) {
 
     updateButtonStates(roles, state_maps, buttonContainer);
     window.onhashchange = () => updateButtonStates(roles, state_maps, buttonContainer);
-    calculatePossessionRate();
 }
 
 function updateButtonStates(roles, state_maps, buttonContainer) {
